0dfa03f6d7b4cf08beb626d27ed2c82c808c5134
Edward Onuoha <72640628+edwardonuoha@users.noreply.github.com>
2024-10-12 16:11:26 +0400

    Add files via upload

---
+++ ReportingApi/MockData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReportingApi
+{
+    public class MockData
+    {
+        public string Name { get; set; }
+        public int Amount { get; set; }
+        public string Password { get; set; }
+    }
+}
